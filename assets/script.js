75c8551298dd81e6df12eb031541c3309706eb0a
millerdotkyle <kylenotfound@gmail.com>
2023-06-12 19:51:50 -0400

    started submit logic
    -created placeholder submit button
    -created listener for click
    -on click both API calls are made and nearest 10 charging stations are logged to console

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+// $( document ).ready(function() {
+    
+// });
+
+
+var geoKey = '_eYJxzg3MbCZnNpEKXWqGRXmDrkUXbnGySiFa7d1T78';
+var evKey = '14df59e4-ba59-4b25-a599-8f0da41f7a88';
+
+
+
+
+
+$('#submit-button').on('click', getstuff);
+
+function getstuff() {
+    //get search terms
+    var addr = $('#address-input').val();
+    var radius = $('#radius-innput').val();
+    //run fetch for geocoordinates
+    fetch('https://geocode.search.hereapi.com/v1/geocode?q='+ addr +'&limit=1&apiKey=' + geoKey)
+    .then(function(response) {
+        return response.json();
+    })
+    //extract coordinates from returned object
+    .then(function(data) {
+        console.log(data);
+        //store coordinates in values
+        var lat = data.items[0].position.lat;
+        var lon = data.items[0].position.lng;
+        console.log(lat);
+        console.log(lon);
+        //run new fetch to chargemap api
+        fetch('https://api.openchargemap.io/v3/poi?key='+ evKey + '&output=json&latitude=' + data.items[0].position.lat + '&longitude=' + data.items[0].position.lng + ' &distance=' + radius + ' &maxresults=10')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            //log final result
+            console.log(data);
+        })
+    })
+}
