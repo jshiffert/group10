d368381e46d69f05ef3dd055f867cdea57273ef0
Jackson Shiffert <jshiffert@oki.org>
2023-06-12 21:21:02 -0400

    combined work from two branches

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var searchHistory = [];
-
+var geoKey = '_eYJxzg3MbCZnNpEKXWqGRXmDrkUXbnGySiFa7d1T78';
+var evKey = '14df59e4-ba59-4b25-a599-8f0da41f7a88';
 
 function storeHistory() {
     // store history from submissions
@@ -43,7 +44,57 @@ function initHistory() {
     renderHistory();
 };
 
-$('#submit-btn').click(function() {
+function getstuff() {
+    //get search terms
+    var addr = $('#address-input').val();
+    var radius = $('#radius-input').val();
+    //run fetch for geocoordinates
+    fetch('https://geocode.search.hereapi.com/v1/geocode?q='+ addr +'&limit=1&apiKey=' + geoKey)
+    .then(function(response) {
+        return response.json();
+    })
+    //extract coordinates from returned object
+    .then(function(data) {
+        // console.log(data);
+        //store coordinates in values
+        // var lat = data.items[0].position.lat;
+        // var lon = data.items[0].position.lng;
+        // console.log(lat);
+        // console.log(lon);
+        //run new fetch to chargemap api
+        fetch('https://api.openchargemap.io/v3/poi?key='+ evKey + '&output=json&latitude=' + data.items[0].position.lat + '&longitude=' + data.items[0].position.lng + ' &distance=' + radius + ' &maxresults=10')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            //log final result
+            console.log(data);
+        })
+    })
+};
+
+function printResults(arr) {
+    //get result element
+    var resultListEl = $('#results-list');
+
+
+    //loop over results, creating elements and adding to list
+    for(i = 0; i< 10; i++) {
+        var a = i+1;
+
+        var resultEl = $('<li>');
+
+        resultEl.text("Result " + a + ": ");
+
+        resultEl.addClass('list-group-item px-3 border-0');
+
+        resultListEl.append(resultEl);
+
+    }
+
+}
+
+function addInput() {
     var addressInput = $('#address-input').val();
     var radiusInput = $('#radius-input').val();
     // return if empty
@@ -59,11 +110,16 @@ $('#submit-btn').click(function() {
     // reset values
     $('#address-input').val("");
     $('#radius-input').val("");
+};
 
+$('#submit-btn').click(function() {
+    getstuff();
+    addInput();
     //store and render new history elements
     storeHistory();
     renderHistory();
 })
 
+printResults();
 // initialize history
 initHistory();
